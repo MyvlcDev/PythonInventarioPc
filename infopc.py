19f60c049e14a0e9cbeb650ac91f37c67e8f9204
unknown <xsync80@myvcl.dev>
2025-03-27 22:06:02 +0100

    actualizando datos

--- infopc.py
+++ infopc.py
@@ -18,23 +18,49 @@ s.connect(("8.8.8.8", 80))
 ip = s.getsockname()[0]
 s.close()
 
+usuario=""
+## placa base
+pb_numeroserie=""
+pb_modelo="" 
+pb_fabricante=""
 
-print("Escribir en la base de datos\n\n")
+## red
+
+dns_pri=""
+dns_sec=""
+puerta_enlace=""
+
+## procesador
+
+cores=""
+
+## hd
 
 
+
+print("Escribir en la base de datossssss\n\n")
+
+print("")
 print(f"Nombre del host: {hostname}")
 print(f"Sistema operativo: {os}")
 print(f"Versión: {version}")
 print(f"Fecha actual: {date}")
 print(f"Ariquitectura: {arquitectura}")
+print("Plataforma:", platform.platform())
+print("Núcleos físicos:", psutil.cpu_count(logical=False))
+print("Núcleos lógicos:", psutil.cpu_count(logical=True))
 print(f"Memoria: {memoria}")
 print(f"Memoria Ram Total: {memoriaRamTotal} GB")
 print(f"IP: {ip}")
 print(f"Dominio: {dominio}")
-print("\n\n")
+##print(f"Usuario actual:", (str)os.getlogin())
 
+print("Discoss Duros:")
+disk_info = psutil.disk_partitions()
+for partition in disk_info:
+    print(f"Disco: {partition.device} - Tipo de sistema de archivos: {partition.fstype}")
+    disk_usage = psutil.disk_usage(partition.mountpoint)
+    print(f"  Espacio total: {disk_usage.total} - Espacio usado: {disk_usage.used} - Espacio libre: {disk_usage.free}")
 
-
-
-
+print("\n")
 
